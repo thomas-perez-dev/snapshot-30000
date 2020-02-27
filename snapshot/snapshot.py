fe477c3edd05be94f7c6e10d77880112c33553ca
thomas-perez-dev <61320965+thomas-perez-dev@users.noreply.github.com>
2020-02-27 13:06:43 +0100

    Only snapshot if there is no snapshot pending

--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -17,6 +17,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 # @ is a decorator
 # Decorator expressions are evaluated when the function is defined
 
@@ -99,6 +103,8 @@ def create_snapshots(project):
         i.wait_until_stopped()
   
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("  Skipping {0} snapshot already in progress".format(v.id))
             print(" Creating snapshots of {0}".format(v.id))
             v.create_snapshot(Description="Created by snapshot 30000")
   
