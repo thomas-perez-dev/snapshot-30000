55ac537d622f6d10642636c1c80444a7e3032be6
thomas-perez-dev <61320965+thomas-perez-dev@users.noreply.github.com>
2020-02-27 09:38:56 +0100

    Filter instances by project. Also, stop and start instances.

--- README.md
+++ README.md
@@ -14,4 +14,7 @@ snapshot uses the configuration file created by the AWS CLI
 
 ## Running
 
-`pipenv run "python snapshot/snapshot.py"`
+`pipenv run python snapshot/snapshot.py <command> <--project=PROJECT>`
+
+*command* is list, start, or stop
+*project* is optional
--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -5,18 +5,68 @@ import click
 session = boto3.Session(profile_name='snapshot')
 ec2 = session.resource('ec2')
 
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
 # @ is a decorator
 # Decorator expressions are evaluated when the function is defined
-@click.command()
-def list_instances():
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
     "List EC2 instances"
-    for i in ec2.instances.all():
+   
+    instances = filter_instances(project)
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            i.public_dns_name,
+            tags.get('Project', '<no project>')
+            )))
+    return
+
+@instances.command('stop')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def stop_instances(project):
+    "Stop EC2 instances"
+
+    instances = filter_instances(project)
+        
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+    
+    return
+
+@instances.command('start')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def start_instances(project):
+    "Start EC2 instances"
+
+    instances = filter_instances(project)
+        
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+    
     return
 
 if __name__ == '__main__':
@@ -26,4 +76,4 @@ if __name__ == '__main__':
  # we use click, more practical
  #  print(sys.argv)
     
-    list_instances()
+    instances()
